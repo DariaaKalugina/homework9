69e37ec670a6fa8c672d543044e37ca75f0d8232
Daria <macdaria1999@gmail.com>
2022-12-25 22:09:34 +0300

    Add task 66

---
+++ task_66/Program.cs
@@ -0,0 +1,12 @@
+﻿int rec(int a, int b)
+{
+    if (a == b)
+        return b;
+    return a + rec(a + 1, b);
+}
+Console.Clear();
+Console.Write("Напишите число 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Напишите число 2: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n, m));
